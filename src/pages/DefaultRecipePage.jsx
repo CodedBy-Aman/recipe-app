2ad687e3198010e4088a88a0f9de0ce3f1c2d27e
Amanpreet Singh <amansg421@gmail.com>
2025-06-11 23:37:07 +0530

    fixed back button navigation issue

--- src/pages/DefaultRecipePage.jsx
+++ src/pages/DefaultRecipePage.jsx
@@ -81,10 +81,10 @@ const handleNext = () => {
             </button>)}
             
             <button
-              onClick={() => navigate('/')}
+              onClick={() => navigate(-1)}
               className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
             >
-              Back to Home
+              Back
             </button>
             {selectedIndex < recipeData.length -1 && ( <button
               onClick={handleNext}
--- src/pages/SingleRecipe.jsx
+++ src/pages/SingleRecipe.jsx
@@ -131,7 +131,7 @@ const SingleRecipe = () => {
 
           <div className="w-full flex justify-center  gap-4 mt-6">
             <button
-              onClick={() => navigate(-1)}
+              onClick={() => navigate("/recipes")}
               className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
             >
               ← Back
